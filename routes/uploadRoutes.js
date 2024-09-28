608e087fe3a391fe0377acab5c2abbda6227295f
masterchiefff <anwarmagara@gmail.com>
2024-09-28 21:48:25 +0300

    vercel added

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -23,7 +23,7 @@ const upload = multer({
 });
 
 // Upload route
-router.post('/upload', upload.single('cv'), async (req, res) => {
+router.post('/upload', authMiddleware, upload.single('cv'), async (req, res) => {
     if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded.' });
     }
